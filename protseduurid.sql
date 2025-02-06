ea8ba066eac908e7655852fe9533103641204ed8
Maria Smolina <mariia.smolina@gmail.com>
2025-02-06 10:13:31 +0200

    Update protseduurid.sql

--- protseduurid.sql
+++ protseduurid.sql
@@ -1,5 +1,7 @@
 -- SQL SALVESTATUD PROTSEDUURID -- Funktsioonid - mitu SQL käsku käivitakse järjest
+-----------------------------------------------------------------------------------
 -- SQL SERVER
+
 CREATE DATABASE procTARgv24;
 
 USE procTARgv24;
@@ -86,4 +88,33 @@ EXEC otsingUudiseTeema 'w';
 
 
 
+-----------------------------------------------------------------------------------
 --XAMPP/localhost
+
+CREATE TABLE uudised(
+uudisID int PRIMARY KEY AUTO_INCREMENT,
+uudiseTeema varchar(50),
+kuupaev date,
+autor varchar(25),
+kirjeldus text
+)
+
+INSERT INTO uudised(uudiseTeema, kuupaev, autor, kirjeldus)
+VALUES(
+'udune ilm', '2025-02-06', 'postimees', 'Lõunani on udune ilm')
+
+--- 
+CREATE PROCEDURE `lisaUudis`(IN `uusTeema` VARCHAR(50), IN `paev` DATE, IN `autor` VARCHAR(25), IN `kirjeldus` TEXT) NOT DETERMINISTIC CONTAINS SQL SQL SECURITY DEFINER BEGIN
+
+INSERT INTO uudised(uudiseTeema, kuupaev, autor, kirjeldus)
+VALUES(
+uusTeema, paev, autor, kirjeldus);
+SELECT * FROM uudised;
+
+END;
+
+CALL lisaUudis ('windows 11', '2025-02-06', 'õpetaja Pant', 'w11 ei tööta multimeedia klassis');
+---
+
+
+
